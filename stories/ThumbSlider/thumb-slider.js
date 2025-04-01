36daab8c3d0006cae9ccecd29c1d9539d9f2551e
mk.concentrix <mikyung.kim@concentrix.com>
2025-04-01 15:20:27 +0900

    thumb slider add

---
+++ stories/ThumbSlider/thumb-slider.js
@@ -0,0 +1,39 @@
+import { register } from 'Swiper/element/bundle';
+register();
+
+
+export class ThumbSlider {
+  constructor(element) {
+    this.element = element;
+    this.init();
+  }
+
+  init() {
+    const mainSliderContainer = this.element.querySelector('.thumbswiper_mainswiper');
+    if (!mainSliderContainer) {
+      console.error('Mainslider container not found');
+      return;
+    }
+
+    const thumbSliderContainer = document.createElement('swiper-container');
+    const thumbSliderClassName = mainSliderContainer.getAttribute('thumbs-swiper').replace('.', '');
+    // const mainSliderClassName = mainSliderContainer.className;
+    thumbSliderContainer.className = thumbSliderClassName;
+    // thumbSliderContainer.setAttribute('controller-control', `.${mainSliderClassName}`);
+    this.element.appendChild(thumbSliderContainer);
+
+    let thumbSlideShow = this.element.getAttribute('thumb-slide-show');
+    if (!thumbSlideShow) {
+      const mainSlideCount = mainSliderContainer.querySelectorAll('swiper-slide').length;
+      thumbSlideShow = mainSlideCount;
+    }
+    thumbSliderContainer.setAttribute('slides-per-view', thumbSlideShow);
+
+    const mainSlides = mainSliderContainer.querySelectorAll('swiper-slide');
+    mainSlides.forEach(slide => {
+      const clonedSlide = slide.cloneNode(true);
+      thumbSliderContainer.appendChild(clonedSlide);
+    });
+
+  }
+}
