7ad9ac5c01fac3239f7720a97df3f67f5ca9f0d4
rarecat85 <81338572+rarecat85@users.noreply.github.com>
2025-04-07 15:19:12 +0900

    [Feature] 썸네일 슬라이드 #11
    > 위치 옵션 추가
    > 썸네일 스와이퍼의 class명 제어할 수 있는 옵션 추가
    > 스토리북에서 옵션 제어해서 테스트할 수 있도록 추가

--- stories/ThumbSlider/thumb-slider.stories.js
+++ stories/ThumbSlider/thumb-slider.stories.js
@@ -25,17 +25,46 @@ ${thumbsliderJs}
         `
       }
     }
+  },
+  argTypes: {
+    thumbSlideShow: {
+      control: 'number',
+      description: '썸네일 슬라이더에 표시될 슬라이드 개수',
+      defaultValue: 4
+    },
+    thumbSlidePosition: {
+      control: 'select',
+      options: ['bottom', 'top', 'left', 'right'],
+      description: '썸네일 슬라이더의 위치',
+      defaultValue: 'bottom'
+    },
+    thumbSwiper: {
+      control: 'text',
+      description: '썸네일 슬라이더의 클래스명',
+      defaultValue: '.thumbswiper_thumbswiper'
+    }
   }
 };
 
 export const Default = {
-  render: () => {
+  render: (args) => {
     const container = document.createElement('div');
     container.innerHTML = thumbsliderHtml;
     
     const sliderElement = container.querySelector('.thumbswiper');
+    
+    // args 값을 속성으로 설정
+    sliderElement.setAttribute('thumb-slide-show', args.thumbSlideShow);
+    sliderElement.setAttribute('thumb-slide-position', args.thumbSlidePosition);
+    sliderElement.querySelector('.thumbswiper_mainswiper').setAttribute('thumbs-swiper', args.thumbSwiper);
+    
     new ThumbSlider(sliderElement);
     
     return container;
+  },
+  args: {
+    thumbSlideShow: 4,
+    thumbSlidePosition: 'bottom',
+    thumbSwiper: '.thumbswiper_thumbswiper'
   }
 }; 
