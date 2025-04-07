c912d8bc2af525309d93792b05527dcf1cf7bd25
yohan.choi <yohanchoi.work@gmail.com>
2025-04-07 14:37:48 +0900

    🔨 storybook Accessibility 기능 활성화 #13

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,6 +7,37 @@ const preview = {
         date: /Date$/i,
       },
     },
+    a11y: {
+      config: {
+        rules: [
+          {
+            // 모든 WCAG 2.1 AA 규칙을 활성화
+            id: 'wcag2a',
+            enabled: true
+          },
+          {
+            id: 'wcag2aa',
+            enabled: true
+          },
+          {
+            id: 'wcag21a',
+            enabled: true
+          },
+          {
+            id: 'wcag21aa',
+            enabled: true
+          }
+        ],
+      },
+      options: {
+        runOnly: {
+          type: 'tag',
+          values: ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'],
+        },
+        shouldShowOnlyHighlightedResults: false,
+      },
+      manual: false,
+    },
   },
 };
 
